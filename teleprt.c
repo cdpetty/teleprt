31b2fa1cbc7aa10bf1621ba065f9152ad222dcc5
Clayton Petty <git.notyalc@gmail.com>
2016-03-27 16:04:53 -0500

    optimization

--- teleprt.c
+++ teleprt.c
@@ -1,17 +1,48 @@
 #include <stdio.h>
 #include <dirent.h>
 #include <string.h>
+#include <stdlib.h>
 
 char* getComponent(char component[], char* path){
-    int iterator = 0;
-    for(iterator; *path != '/' && *path; component[iterator] = *path++, iterator++){}
+    int iterator;
+    char* pathPointer = path;
+    for(iterator = 0; *pathPointer != '/' && *pathPointer; iterator++, pathPointer++);
+//    printf("Iterator: %d\n", iterator);
+//    strncpy(component, path, iterator);
+    memcpy(component, path, iterator);
     component[iterator] = '\0';
-    if (*path == '\0'){
-        return path;
-    } else {
-        return ++path;
-    }
+//    printf("Component: %s\n", component);
+//    fflush(0);
+    return (*pathPointer == '\0') ? pathPointer : ++pathPointer;
+//    if (*pathPointer == '\0'){
+//        printf("returning pathpointer: %s\n", pathPointer);
+//        return pathPointer;
+//    } else {
+//        printf("returning pathpointer: %s\n", ++pathPointer);
+////        return ++pathPointer;
+//        return pathPointer;
+//    }
 }
+
+//char* getComponent(char component[], char* path){
+//    int iterator;
+//    for(iterator = 0; *path != '/' && *path; component[iterator] = *path++, iterator++){}
+//    component[iterator] = '\0';
+//    if (*path == '\0'){
+//        return path;
+//    } else {
+//        return ++path;
+//    }
+//}
+
+//char* getComponent(char component[], char* path){
+////    printf("Called get component with compoentn %s and path %s", component, path);
+//    memccpy(component, path, '/', strlen(path));
+//    component[strlen(component)-1] = '\0';
+////    printf("\ncomponent: %s\n", component);
+//    return (*(path + strlen(component))) ? path + strlen(component) + 1 : path + strlen(component);
+//}
+
 
 int doesMatch(char component[], char* name){
     char* tmp = component;
@@ -24,40 +55,52 @@ char* getDir(char component[], char* name){
     struct dirent *de;
 
     if (!(directory = opendir(name))){
-//        printf("Error opening directory");
+        fputs("TP error: cannot open directory", stderr);
+        exit(1);
     }
 
-    while (de = readdir(directory)){
+    while ( (de = readdir(directory)) ){
         if (de->d_type == DT_DIR) {
+//            puts(de->d_name);
             if (doesMatch(component, de->d_name) == 0) {
                 closedir(directory);
                 return de->d_name;
             }
         }
     }
-
+    return NULL;
 }
 
 int main(int argc, char **argv, char **envp){
 
-    char component[30];
-    char* path = *(++argv);
-
-    char realPath[70];
-    bzero(realPath, sizeof(realPath));
-    if (*path == '/'){
-        strcat(realPath, "/.");
-        path++;
+    if (argc == 1){
+        puts(getenv("HOME"));
     } else {
-        strcat(realPath, ".");
-    }
+        char component[30];
+        char* path = *(++argv);
 
-    while (*path) {
-        path = getComponent(component, path);
-        strcat(realPath, "/");
-        strcat(realPath, getDir(component, realPath));
-    }
+        char realPath[70];
+        bzero(realPath, sizeof(realPath));
+        if (*path == '/'){
+            strcat(realPath, "/.");
+            path++;
+        } else {
+            strcat(realPath, ".");
+        }
 
-    puts(realPath);
+        while (*path) {
+            path = getComponent(component, path);
+            strcat(realPath, "/");
+            char* newDir = getDir(component, realPath);
+            if (newDir){
+                strcat(realPath, newDir);
+            } else {
+                puts(".");
+                return 0;
+            }
+        }
+
+        puts(realPath);
+    }
     return 0;
 }
